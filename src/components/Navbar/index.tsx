c69422d40da51884be3d840854595b7e9e889ff8
paulori22 <paulinhoboneti@yahoo.com.br>
2024-03-06 20:51:59 -0300

    Creating Input Search - Lesson 5 - Created InputSearch component and added on the NavBar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
+import { AppBar, Grid, Toolbar, makeStyles } from "@material-ui/core";
 import Title from "./Title";
 import Notifications from "./Notifications";
 import MenuCategories from "../menus/MenuCategories";
 import MenuAccount from "../menus/MenuAccount";
+import InputSearch from "../InputSearch";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,6 +20,11 @@ const NavBar: React.FunctionComponent = () => {
         <Toolbar>
           <MenuCategories />
           <Title />
+          <Grid container justify="center">
+            <Grid item xs={8} lg={7}>
+              <InputSearch />
+            </Grid>
+          </Grid>
           <Notifications />
           <MenuAccount />
         </Toolbar>
