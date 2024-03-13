5e782acaa019508f315dfe4c9abfe14c3352122d
paulori22 <paulinhoboneti@yahoo.com.br>
2024-03-13 20:26:17 -0300

    Creating hook to check mobile size and custom breakpoints - Lesson 6

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import Notifications from "./Notifications";
 import MenuCategories from "../menus/MenuCategories";
 import MenuAccount from "../menus/MenuAccount";
 import InputSearch from "../InputSearch";
+import useIsSmallWindow from "../../hooks/useIsSmallWindow";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,20 +14,25 @@ const useStyles = makeStyles((theme) => ({
 
 const NavBar: React.FunctionComponent = () => {
   const classes = useStyles();
+  const isSmallWindow = useIsSmallWindow();
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <MenuCategories />
+          {!isSmallWindow && <MenuCategories />}
           <Title />
-          <Grid container justify="center">
-            <Grid item xs={8} lg={7}>
-              <InputSearch />
-            </Grid>
-          </Grid>
-          <Notifications />
-          <MenuAccount />
+          {!isSmallWindow && (
+            <>
+              <Grid container justify="center">
+                <Grid item xs={8} lg={7}>
+                  <InputSearch />
+                </Grid>
+              </Grid>
+              <Notifications />
+              <MenuAccount />
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>
--- src/theme.ts
+++ src/theme.ts
@@ -20,6 +20,16 @@ const palette: PaletteOptions = {
 
 const theme = createMuiTheme({
   palette,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      mobile: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
   overrides: {
     MuiAppBar: {
       colorPrimary: {
